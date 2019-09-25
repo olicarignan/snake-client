98e30b8d7ac75b649994b572453293edd1d967e5
olicarignan <olive.carignan@gmail.com>
2019-09-25 16:55:45 +0000

    implement name

--- client.js
+++ client.js
@@ -1,12 +1,18 @@
 const net = require('net');
 
-const connect = function(client) {
+const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.0.103',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('connection successful');
+    conn.write('Name: oli');
+  });
+
 
   conn.on('data', () => {
     console.log('you ded cuz you idled')
